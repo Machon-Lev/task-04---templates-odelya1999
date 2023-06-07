0f9debf301557c2108509b2b73a71c3afedc4334
Odelya yakowich <odelyaha1998@gmail.com>
2023-06-08 00:34:45 +0300

    add base file

---
+++ PriorityQueue/include/PriorityQueue.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <list>
+#include <exception>
+
+
+// any templates?
+class PriorityQueue {
+   
+public:
+	
+   // You need to complete the implement : 
+	void push(const T& t); 
+	T poll();
+
+private:
+// add relevant data members
+};
