468b4614eb9b2150256e81ee07568dae62642210
Odelya yakowich <odelyaha1998@gmail.com>
2023-06-08 19:26:54 +0300

    add Dog class

--- PriorityQueue/include/Dog.h
+++ PriorityQueue/include/Dog.h
@@ -1,3 +1,8 @@
+/**************************
+Name: Odelya Yaakowich
+ID: 207346784
+****************************/
+
 #pragma once
 #include <iostream>
 #include <string>
@@ -26,4 +31,4 @@ ostream& operator<< (ostream& os, Dog d);
 
 Dog operator-(const Dog& lhs, const Dog& rhs);
 
-bool operator<(const Dog& lhs, const int rhs);
+bool operator<(const Dog& lhs, const Dog& rhs);
